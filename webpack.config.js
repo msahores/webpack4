ee0596e30dbed622e70b275e49284d0ded89bc5a
lubuntu <manuel@sahores.com>
2019-07-25 23:59:30 -0300

    Fonts loading configuration

--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,17 @@ module.exports = {
                  }
                }
              ]
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+               {
+                 loader: "file-loader",
+                 options: {
+                   outputPath: 'fonts'
+                 }
+               }
+             ]
       }
     ]
   },
