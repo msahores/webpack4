2f59e6d793fdd1da86649a45c91d468fe2505b85
Manuel Sahores <manuel.sahores@rga.com>
2019-07-26 12:03:21 -0300

    asset updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,75 @@
-const path = require('path');
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-
-  entry: './src/javascript/index.js',
+  entry: "./src/javascript/index.js",
 
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js"
   },
 
-  mode: 'development',
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
+            presets: ["@babel/preset-env"]
           }
         }
       },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
-                {
-                  loader: MiniCssExtractPlugin.loader
-                },
-               {
-                 loader: "css-loader",
-               },
-               {
-                 loader: "postcss-loader"
-               },
-               {
-                 loader: "sass-loader",
-                 options: {
-                   implementation: require("sass")
-                 }
-               }
-             ]
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          }
+        ]
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/,
         use: [
-               {
-                 loader: "file-loader",
-  
-                 options: {
-                   outputPath: 'images'
-                 }
-               }
-             ]
+          {
+            loader: "file-loader",
+
+            options: {
+              outputPath: "images"
+            }
+          }
+        ]
       },
       {
         test: /\.(woff|woff2|ttf|otf|eot)$/,
         use: [
-               {
-                 loader: "file-loader",
-                 options: {
-                   outputPath: 'fonts'
-                 }
-               }
-             ]
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "fonts"
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
-
     new MiniCssExtractPlugin({
-      filename: "bundle.css"
+      filename: "css/bundle.css"
     })
-  
   ]
 };
